7f40c04b0abbb828a1c8ab599df8cba3ef54154c
ShravyaHub <shravyak514@gmail.com>
2020-10-14 22:07:55 +0530

    Added file: SortByFirstName.java

---
+++ SortByFirstName.java
@@ -0,0 +1,12 @@
+import java.util.Comparator;
+
+public class SortByFirstName implements Comparator<Person> {
+
+        @Override
+        public int compare(Person person1, Person person2) {
+
+            return (person1.firstName).compareTo(person2.lastName);
+
+        }
+
+}
