a6287cedb786aa4a71b13a7352887a190e861d80
ShravyaHub <shravyak514@gmail.com>
2020-10-14 20:07:03 +0530

    Added file: SortByZip.java

--- SortByZip.java
+++ SortByZip.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class SortByZip {
+import java.util.Comparator;
+
+public class SortByZip implements Comparator<Person> {
+
+    @Override
+    public int compare(Person person1, Person person2) {
+
+        return (person1.zip).compareTo(person2.zip);
+
+    }
+
 }
