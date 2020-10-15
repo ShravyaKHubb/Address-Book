5a4ac44037a63a0b9db907027cd935193b45898a
ShravyaHub <shravyak514@gmail.com>
2020-10-15 16:05:59 +0530

    Added file: AddressBookMain.java

---
+++ AddressBookMain.java
@@ -0,0 +1,12 @@
+public class AddressBookMain {
+
+    //Welcome to Address Book Program
+
+    public static void main(String[] args) {
+
+        AddressBookOperations addressBookOperations = new AddressBookOperations();
+        addressBookOperations.addPerson();
+
+    }
+
+}
