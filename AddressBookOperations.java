d120117253a9c920cedd3f8ce58ffdffc9ae3804
ShravyaHub <shravyak514@gmail.com>
2020-10-15 16:06:34 +0530

    Added file: AddressBookOperations.java

---
+++ AddressBookOperations.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class AddressBookOperations implements AddressBookInterface{
+
+    @Override
+    public void addPerson() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first name: ");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter last name: ");
+        String lastName = scanner.nextLine();
+        System.out.println("Enter address : ");
+        String address = scanner.nextLine();
+        System.out.println("Enter city: ");
+        String city = scanner.nextLine();
+        System.out.println("Enter state: ");
+        String state = scanner.nextLine();
+        System.out.println("Enter zip: ");
+        String zip = scanner.nextLine();
+        System.out.println("Enter phone number: ");
+        String phoneNumber = scanner.nextLine();
+        new Person(firstName, lastName, address, city, state, zip, phoneNumber);
+        System.out.println("Contact added!");
+
+    }
+
+}
