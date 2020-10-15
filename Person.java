5bf9ec381378b84507bb2fd1d480cc9902da38d1
ShravyaHub <shravyak514@gmail.com>
2020-10-15 16:07:20 +0530

    Added file: Person.java

---
+++ Person.java
@@ -0,0 +1,17 @@
+public class Person {
+
+    String firstName, lastName, address, city, state, zip, phoneNumber;
+
+    Person(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber){
+
+        this.firstName=firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+
+    }
+
+}
