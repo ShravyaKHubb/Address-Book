c801094345dc164845584ea0e1e13e5396a7a6b6
ShravyaHub <shravyak514@gmail.com>
2020-10-15 16:41:07 +0530

    Added file: AddressBookOperations.java

---
+++ AddressBookOperations.java
@@ -0,0 +1,117 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBookOperations implements AddressBookInterface {
+
+    String firstName, lastName, address, city, state, zip, phoneNumber;
+    ArrayList<Person> contacts = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+    Person personInfo;
+
+    public void addPerson() {
+
+        System.out.println("Enter first name: ");
+        firstName = scanner.nextLine();
+        System.out.println("Enter last name: ");
+        lastName = scanner.nextLine();
+        System.out.println("Enter address : ");
+        address = scanner.nextLine();
+        System.out.println("Enter city: ");
+        city = scanner.nextLine();
+        System.out.println("Enter state: ");
+        state = scanner.nextLine();
+        System.out.println("Enter zip: ");
+        zip = scanner.nextLine();
+        System.out.println("Enter phone number: ");
+        phoneNumber = scanner.nextLine();
+        personInfo = new Person(firstName, lastName, address, city, state, zip, phoneNumber);
+        contacts.add(personInfo);
+        System.out.println("Contact added!");
+
+    }
+
+    public void editPerson() {
+
+        System.out.print("Enter first name: ");
+        firstName = scanner.nextLine();
+        System.out.println("Enter last name: ");
+        lastName = scanner.nextLine();
+
+        for (Person editPersonInfo : contacts) {
+
+            if (firstName.equals(editPersonInfo.firstName) && lastName.equals(editPersonInfo.lastName)) {
+
+                System.out.println("Edit:\n 1.Address\n 2.City\n 3.State\n 4.Zip\n 5.Phone number ");
+                int choice = scanner.nextInt();
+
+                switch (choice) {
+
+                    case 1:
+                        System.out.println("Enter new address: ");
+                        scanner.nextLine();
+                        address = scanner.nextLine();
+                        editPersonInfo.address = address;
+                        break;
+
+                    case 2:
+                        System.out.println("Enter new city: ");
+                        scanner.nextLine();
+                        city = scanner.nextLine();
+                        editPersonInfo.city = city;
+                        break;
+
+                    case 3:
+                        System.out.println("Enter new state: ");
+                        scanner.nextLine();
+                        state = scanner.nextLine();
+                        editPersonInfo.state = state;
+                        break;
+
+                    case 4:
+                        System.out.println("Enter new zip: ");
+                        scanner.nextLine();
+                        zip = scanner.nextLine();
+                        editPersonInfo.zip = zip;
+                        break;
+
+                    case 5:
+                        System.out.println("Enter new phone number: ");
+                        scanner.nextLine();
+                        phoneNumber = scanner.nextLine();
+                        editPersonInfo.phoneNumber = phoneNumber;
+                        break;
+
+                    default:
+                        System.out.println("Invalid input");
+
+                }
+
+            }
+
+        }
+
+    }
+
+    public void deletePerson() {
+
+        for (int i = 0; i < contacts.size(); i++) {
+
+            personInfo = contacts.get(i);
+            System.out.println("Enter first name: ");
+            firstName = scanner.nextLine();
+            System.out.println("Enter last name: ");
+            lastName = scanner.nextLine();
+
+            if (firstName.equals(personInfo.firstName) && lastName.equals(personInfo.lastName)) {
+
+                contacts.remove(i);
+
+            }
+
+        }
+
+        System.out.println("Contact deleted!");
+
+    }
+
+}
